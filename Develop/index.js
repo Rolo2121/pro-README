27635e0fc0565bba33ce3adf7491ecdcc7f11216
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-05-20 14:53:49 -0500

    created prompts in terminal to ask user questions needed to file out the ReadMe

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,93 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
+console.log('Welcome to the README generator!')
+
+ 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('Please enter your project name!');
+                        return false;
+                    }
+                }
+        },
+        {
+            type: 'input',
+            name: 'discription',
+            message: 'Please discribe your project! (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('Please enter a description of your project!');
+                    }
+                }
+        
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Please enter Installation Instructions (Required) :', 
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('Please enter installation instructions');
+                    }
+            }
+             
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please enter usage information (Required) :',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('Please enter a description of the usage of this project');
+                    }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Please enter any contributors (Required) :', 
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('If no contributors type: "none"');
+                    }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please enter how you want to test your project',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log('Please enter how to test your project!');
+                }
+            }
+        }
+    ])
+}
 
+promptUser()
+   
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+// const fs = require('fs/promises');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
